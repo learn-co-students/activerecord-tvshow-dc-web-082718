91aaf95cd2fb3c521e95d845cbf29321389aa873
kruchkamgar <kruchkamgar@gmail.com>
2018-09-09 15:32:26 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,36 @@
+# require 'pry'
+
+class Show < ActiveRecord::Base
+
+	def self.highest_rating
+
+		Show.maximum(:rating)
+
+	end
+
+	def self.most_popular_show
+		Show.order(:rating).last
+	end
+
+	def self.lowest_rating
+		Show.minimum(:rating)
+	end
+
+	def self.least_popular_show
+		Show.order(:rating).first
+	end
+
+	def self.ratings_sum
+		self.sum(:rating)
+	end
+
+	def self.popular_shows
+		self.where('rating > 5')
+
+	end
+
+	def self.shows_by_alphabetical_order
+		self.order(name: :asc)
+	end
+
+end
